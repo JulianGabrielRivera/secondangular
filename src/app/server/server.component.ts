871893ad551608c5127dac0032337d8c9b870b69
Julian Rivera <julianarevircoins@gmail.com>
2022-06-20 02:18:53 -0400

    the basics

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   // type not required but we can use them
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   // method
   getServerStatus() {
     return this.serverStatus;
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreated = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test';
+  username = 'yo';
+  servers = ['Testserver', 'Testserver 2'];
+  paragraph = false;
+  paragraphtext = 'Hello World!';
+  log: any[] = [];
+
   constructor() {
     // the .this keyword works on the arrow function syntax.
     setTimeout(() => {
@@ -19,10 +26,21 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: any) {
     this.serverName = event.target.value;
+  }
+  onUpdateUserName() {
+    this.username = '';
+  }
+  onToggle() {
+    this.paragraph = !this.paragraph;
+    // we get a never issue here if we dont put log the way it is.
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
